fbd85dcc12871da14c93dee2f74f271adeb4f0d5
kwong25 <kwong25@u.rochester.edu>
2021-08-05 15:27:27 -0400

    changed indents in the html for structure readability

--- phoenix.php
+++ phoenix.php
@@ -8,134 +8,134 @@
 
 	<body>
 
-	<div class="container">
+		<div class="container">
 
-		<header>
-			<h1>Phoenix, AZ</h1>
-			<img src="https://placebear.com/1250/256">
-		</header>
+			<header>
+				<h1>Phoenix, AZ</h1>
+				<img src="https://placebear.com/1250/256">
+			</header>
 
-		<navbar class="menu">
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="chicago.php">Chicago</a></li>
-				<li><a href="phoenix.php">Phoenix</a></li>
-				<li><a href="ridgewood.php">Ridgewood</a></li>
-			</ul>
-		</navbar>
+			<navbar class="menu">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="chicago.php">Chicago</a></li>
+					<li><a href="phoenix.php">Phoenix</a></li>
+					<li><a href="ridgewood.php">Ridgewood</a></li>
+				</ul>
+			</navbar>
 
-		<article>
-			<h2>Quick Facts</h2>
-			<img src="https://placebear.com/300/400">
+			<article>
+				<h2>Quick Facts</h2>
+				<img src="https://placebear.com/300/400">
 
-			<h3>Population: 4.5 million</h3>
-			<h3>Geography</h3>
-			<ul>
-				<li>State: Arizona</li>
-				<li>Region: Southwest</li>
-				<li>Size: 517 square miles</li>
-			</ul>
+				<h3>Population: 4.5 million</h3>
+				<h3>Geography</h3>
+				<ul>
+					<li>State: Arizona</li>
+					<li>Region: Southwest</li>
+					<li>Size: 517 square miles</li>
+				</ul>
 
-			<h2>History of Phoenix</h2>
+				<h2>History of Phoenix</h2>
 
-			<h3>City's Founding</h3>
-			<p>Mollis. Et potenti pede hac faucibus mi per nascetur gravida sed urna aliquet montes pellentesque primis rhoncus pharetra. Suspendisse cursus parturient, interdum eget dapibus mollis ad conubia accumsan neque iaculis. Magnis sodales ultricies. Lorem eget dignissim senectus ullamcorper fusce consectetuer. Nisi felis. Aenean rhoncus suscipit viverra vulputate litora. Sem. Aenean.</p>
+				<h3>City's Founding</h3>
+				<p>Mollis. Et potenti pede hac faucibus mi per nascetur gravida sed urna aliquet montes pellentesque primis rhoncus pharetra. Suspendisse cursus parturient, interdum eget dapibus mollis ad conubia accumsan neque iaculis. Magnis sodales ultricies. Lorem eget dignissim senectus ullamcorper fusce consectetuer. Nisi felis. Aenean rhoncus suscipit viverra vulputate litora. Sem. Aenean.</p>
 
-			<h3>Famous Historical Events</h3>
-			<p>Lacus cum fringilla rhoncus magnis dis ullamcorper fringilla, elit pede. Nulla curabitur fermentum. Fringilla class ipsum. Tellus mauris magna proin facilisis porta maecenas metus suscipit aenean quisque aliquam. Non aliquet nullam aptent. Elementum ligula fermentum erat at dolor suscipit est ipsum dictumst eleifend Magnis aliquam purus fringilla fusce facilisis morbi.</p>
+				<h3>Famous Historical Events</h3>
+				<p>Lacus cum fringilla rhoncus magnis dis ullamcorper fringilla, elit pede. Nulla curabitur fermentum. Fringilla class ipsum. Tellus mauris magna proin facilisis porta maecenas metus suscipit aenean quisque aliquam. Non aliquet nullam aptent. Elementum ligula fermentum erat at dolor suscipit est ipsum dictumst eleifend Magnis aliquam purus fringilla fusce facilisis morbi.</p>
 
-			<h2>What's it like in Phoenix?</h2>
-			<h3>Landscape</h3>
-			<p>Nunc senectus habitasse Massa lectus ligula tortor hac. Penatibus, primis imperdiet velit. Facilisis duis. Hac blandit vestibulum pretium etiam pellentesque diam dictum. Mi magnis Viverra nunc cum mus. Odio curabitur, dignissim, mauris Nibh cum magnis conubia semper feugiat mauris posuere vitae adipiscing mi cras, potenti lacinia ut imperdiet enim rhoncus.</p>
+				<h2>What's it like in Phoenix?</h2>
+				<h3>Landscape</h3>
+				<p>Nunc senectus habitasse Massa lectus ligula tortor hac. Penatibus, primis imperdiet velit. Facilisis duis. Hac blandit vestibulum pretium etiam pellentesque diam dictum. Mi magnis Viverra nunc cum mus. Odio curabitur, dignissim, mauris Nibh cum magnis conubia semper feugiat mauris posuere vitae adipiscing mi cras, potenti lacinia ut imperdiet enim rhoncus.</p>
 
-			<h3>Weather</h3>
-			<p>Phoenix is famous for being very hot and very dry.  Phoenix's average July high is 105 degrees, with winter "cooling" the city down to an average January low of 43 degrees.  Phoenix only receives about 9 inches of rain per year, a quarter of the US average.</p>
+				<h3>Weather</h3>
+				<p>Phoenix is famous for being very hot and very dry.  Phoenix's average July high is 105 degrees, with winter "cooling" the city down to an average January low of 43 degrees.  Phoenix only receives about 9 inches of rain per year, a quarter of the US average.</p>
 
-			<img src="https://placebear.com/500/300">
-			
-		</article>
+				<img src="https://placebear.com/500/300">
+				
+			</article>
 
-		<aside>
-			<h2>Visit Phoenix</h2>
-			<h3>Places to See</h3>
-				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
-				</figure>
+			<aside>
+				<h2>Visit Phoenix</h2>
+				<h3>Places to See</h3>
+					<figure>
+						<img src="https://placebear.com/250/250" alt="image description">
+						<figcaption>figure caption</figcaption>
+					</figure>
 
-				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
-				</figure>
+					<figure>
+						<img src="https://placebear.com/250/250" alt="image description">
+						<figcaption>figure caption</figcaption>
+					</figure>
 
-				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
-				</figure>
+					<figure>
+						<img src="https://placebear.com/250/250" alt="image description">
+						<figcaption>figure caption</figcaption>
+					</figure>
 
-			<h3>Places to Eat</h3>
-				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
-				</figure>
+				<h3>Places to Eat</h3>
+					<figure>
+						<img src="https://placebear.com/250/250" alt="image description">
+						<figcaption>figure caption</figcaption>
+					</figure>
 
-				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
-				</figure>
+					<figure>
+						<img src="https://placebear.com/250/250" alt="image description">
+						<figcaption>figure caption</figcaption>
+					</figure>
 
-				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
-				</figure>
+					<figure>
+						<img src="https://placebear.com/250/250" alt="image description">
+						<figcaption>figure caption</figcaption>
+					</figure>
 
 				<h3>Places to Stay</h3>
-				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
-				</figure>
+					<figure>
+						<img src="https://placebear.com/250/250" alt="image description">
+						<figcaption>figure caption</figcaption>
+					</figure>
 
-				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
-				</figure>
+					<figure>
+						<img src="https://placebear.com/250/250" alt="image description">
+						<figcaption>figure caption</figcaption>
+					</figure>
 
-				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
-				</figure>
-			
-			<!-- FPO map -->
-			<iframe src="https://www.google.com/maps/d/u/1/embed?mid=1h18b6_6lk0zOcIDRPwTRwqqiOuV2BBhX" width="300" height="300"></iframe>
+					<figure>
+						<img src="https://placebear.com/250/250" alt="image description">
+						<figcaption>figure caption</figcaption>
+					</figure>
+				
+				<!-- FPO map -->
+				<iframe src="https://www.google.com/maps/d/u/1/embed?mid=1h18b6_6lk0zOcIDRPwTRwqqiOuV2BBhX" width="300" height="300"></iframe>
 
-			<h2>Sign the Guest Book<h2>
-			<form method="post" action="">
+				<h2>Sign the Guest Book<h2>
+				<form method="post" action="">
 
-			<label for="firstName">First Name:</label>
-			<input type="text" name="firstName"> <br> <br>
+				<label for="firstName">First Name:</label>
+				<input type="text" name="firstName"> <br> <br>
 
-			<label for="lastname">Last Name:</label>
-			<input type="text" name="lastname" id="lastname"> <br> <br>
+				<label for="lastname">Last Name:</label>
+				<input type="text" name="lastname" id="lastname"> <br> <br>
 
-			<label for="email">Email:</label>
-			<input type="text" name="email" id="email"> <br> <br>
+				<label for="email">Email:</label>
+				<input type="text" name="email" id="email"> <br> <br>
 
-			<label for="phoneNumber">Zipcode:</label>
-			<input type="text" name="zipcode" id="zipcode"> <br> <br>
+				<label for="phoneNumber">Zipcode:</label>
+				<input type="text" name="zipcode" id="zipcode"> <br> <br>
 
-			<label for="phoneNumber">City Visited:</label>
-			<input type="text" name="city" id="city"> <br> <br>
+				<label for="phoneNumber">City Visited:</label>
+				<input type="text" name="city" id="city"> <br> <br>
 
-			<div class = "submit"><input type="submit" value="Signed!"></div>
-			
-		</aside>
+				<div class = "submit"><input type="submit" value="Signed!"></div>
+				
+			</aside>
 
-		<footer>
-			<p>Phoenix Copyright 2021</p>
-			<p>CSC 174 Advanced Front End Web Development</p>
-		</footer>
+			<footer>
+				<p>Phoenix Copyright 2021</p>
+				<p>CSC 174 Advanced Front End Web Development</p>
+			</footer>
 
-	</div>
+		</div>
 
 	</body>
 
