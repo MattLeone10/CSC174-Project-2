4a07ad7c57082d712b3b60d6d9044f7da95c708f
Miranda Price <miranda27price@gmail.com>
2021-08-05 21:30:02 -0500

    added visit phoenix content

--- phoenix.php
+++ phoenix.php
@@ -27,7 +27,7 @@
 		<article>
 			<h2>Quick Facts</h2>
 			<img src="https://placebear.com/300/400">
-			
+
 			<ul>
 				<li>State: Arizona</li>
 				<li>Region: Southwest</li>
@@ -58,50 +58,50 @@
 			<h2>Visit Phoenix</h2>
 			<h3>Places to See</h3>
 				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
+					<img src="images/botanical.jpg" alt="A sculpture of a pink head on a blue pedestal surrounded by cacti outside">
+					<figcaption>Desert Botanical Garden</figcaption>
 				</figure>
 
 				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
+					<img src="images/lost_dutchman.jpg" alt="A large rock structure in the desert with a cactus to the right.">
+					<figcaption>Lost Dutchman State Park</figcaption>
 				</figure>
 
 				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
+					<img src="images/scottsdale.jpg" alt="Three buildings styled like the Old West with three cars parked in front of them and metal cowboy sculptures to the right.">
+					<figcaption>Scottsdale Old Town</figcaption>
 				</figure>
 
 			<h3>Places to Eat</h3>
 				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
+					<img src="images/ghost_ranch.jpg" alt="A dining area with wooden tables, yellow walls, and southwest inspired decorations.">
+					<figcaption>Ghost Ranch</figcaption>
 				</figure>
 
 				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
+					<img src="images/kai.jpg" alt="A spacious dining area with a gray and white interior and large windows.">
+					<figcaption>Kai</figcaption>
 				</figure>
 
 				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
+					<img src="images/bbq.jpg" alt="One of the famous long lines for Little Miss BBQ waiting outside the building.">
+					<figcaption>Little Miss BBQ</figcaption>
 				</figure>
 
 				<h3>Places to Stay</h3>
 				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
+					<img src="images/san_carlos.jpg" alt="A large and old-fashioned hotel building with a sign that says San Carlos Hotel.">
+					<figcaption>Hotel San Carlos</figcaption>
 				</figure>
 
 				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
+					<img src="images/biltmore.jpg" alt="A large brick building with many sections and stories">
+					<figcaption>Arizona Biltmore</figcaption>
 				</figure>
 
 				<figure>
-					<img src="https://placebear.com/250/250" alt="image description">
-					<figcaption>figure caption</figcaption>
+					<img src="images/hi_phoenix.jpg" alt="A hostel room with two sets of bunk beds with colorful bedding">
+					<figcaption>HI Phoenix</figcaption>
 				</figure>
 			
 			<!-- FPO map -->
