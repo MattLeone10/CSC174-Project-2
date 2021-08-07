5408439cec3830ed41180c31295d66cd4943ed24
Miranda Price <miranda27price@gmail.com>
2021-08-07 11:54:31 -0500

    added homepage city descriptions

--- index.php
+++ index.php
@@ -24,21 +24,21 @@
 			<h2>Our Cities:</h2>
 
 			<a href="chicago.php">
-				<!-- background image to represent Phoenix -->
+				<!-- background image to represent Chicago -->
 				<h3>Chicago, IL</h3>
-				<p>Aenean malesuada Semper curabitur ipsum cum inceptos platea dignissim consequat rhoncus pede dolor. Pulvinar vel varius. Massa elementum litora nulla quis, penatibus fusce nullam magnis parturient dignissim aliquet Dignissim turpis.</p>
+				<p>The third largest city in the world, Chicago sits on the shore of Lake Michigan and has plenty of museums, parks, and other attractions to offer.</p>
 			</a>
 
 			<a href="phoenix.php">
 				<!-- background image to represent Phoenix -->
 				<h3>Phoenix, AZ</h3>
-				<p>Aenean malesuada Semper curabitur ipsum cum inceptos platea dignissim consequat rhoncus pede dolor. Pulvinar vel varius. Massa elementum litora nulla quis, penatibus fusce nullam magnis parturient dignissim aliquet Dignissim turpis.</p>
+				<p>Located in the Arizona desert, Phoenix is home to natural wonders and hotel resorts alike and offers some of the best Southwestern food and art.</p>
 			</a>
 
 			<a href="ridgewood.php">
-				<!-- background image to represent Phoenix -->
+				<!-- background image to represent Ridgewood -->
 				<h3>Ridgewood, NJ</h3>
-				<p>Aenean malesuada Semper curabitur ipsum cum inceptos platea dignissim consequat rhoncus pede dolor. Pulvinar vel varius. Massa elementum litora nulla quis, penatibus fusce nullam magnis parturient dignissim aliquet Dignissim turpis.</p>
+				<p>Located near Manhattan with a ridge providing a view of the city, Ridgewood is home to several parks, a schoolhouse museum, and a landscape architectural center.</p>
 			</a>
 
 		</article>
