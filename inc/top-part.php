ae0193319b69514606d957c7cc8d30bba3b46284
MattLeone10 <mleone10@u.rochester.edu>
2021-08-10 20:07:07 -0400

    added in menu highlighter

--- inc/top-part.php
+++ inc/top-part.php
@@ -5,6 +5,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>CSC 174 | Project 2</title>
+		<link rel="stylesheet" href="css/styles.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 
