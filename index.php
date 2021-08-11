f101049054e94d50d272aaf1c9226d39340c5d6b
kwong25 <kwong25@u.rochester.edu>
2021-08-10 21:04:43 -0400

    put article content into columns

--- index.php
+++ index.php
@@ -16,24 +16,30 @@ include "inc/top-part.php";
 		<article>
 			<h2>Our Cities:</h2>
 
-			<a href="chicago.php">
-				<!-- background image to represent Chicago -->
-				<h3>Chicago, IL</h3>
-				<p>The third largest city in the world, Chicago sits on the shore of Lake Michigan and has plenty of museums, parks, and other attractions to offer.</p>
-			</a>
+			<div class="col-4">
+				<a href="chicago.php">
+					<!-- background image to represent Chicago -->
+					<h3>Chicago, IL</h3>
+					<p>The third largest city in the world, Chicago sits on the shore of Lake Michigan and has plenty of museums, parks, and other attractions to offer.</p>
+				</a>
+			</div>
 
-			<a href="phoenix.php">
-				<!-- background image to represent Phoenix -->
-				<h3>Phoenix, AZ</h3>
-				<p>Located in the Arizona desert, Phoenix is home to natural wonders and hotel resorts alike and offers some of the best Southwestern food and art.</p>
-			</a>
+			<div class="col-4">
+				<a href="phoenix.php">
+					<!-- background image to represent Phoenix -->
+					<h3>Phoenix, AZ</h3>
+					<p>Located in the Arizona desert, Phoenix is home to natural wonders and hotel resorts alike and offers some of the best Southwestern food and art.</p>
+				</a>
+			</div>
 
-			<a href="ridgewood.php">
-				<!-- background image to represent Ridgewood -->
-				<h3>Ridgewood, NJ</h3>
-				<p>Located near Manhattan with a ridge providing a view of the city, Ridgewood is home to several parks, a schoolhouse museum, and a landscape architectural center.</p>
-			</a>
-
+			<div class="col-4">
+				<a href="ridgewood.php">
+					<!-- background image to represent Ridgewood -->
+					<h3>Ridgewood, NJ</h3>
+					<p>Located near Manhattan with a ridge providing a view of the city, Ridgewood is home to several parks, a schoolhouse museum, and a landscape architectural center.</p>
+				</a>
+			</div>
+			
 		</article>
 		<h2>Sign the Guest Book<h2>
 		<?php include "scripts/insert.php";?>
