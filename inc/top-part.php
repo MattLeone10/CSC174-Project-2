c1d413f6595a57f2c88c50125079003fa128eaef
kwong25 <kwong25@u.rochester.edu>
2021-08-11 21:03:13 -0400

    renamed style sheet for all custom styles and override styles

--- inc/top-part.php
+++ inc/top-part.php
@@ -6,7 +6,7 @@
 		<meta charset="utf-8">
 		<title>CSC 174 | Project 2</title>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-		<link rel="stylesheet" href="css/styles.css">
+		<link rel="stylesheet" href="css/custom-styles.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 
