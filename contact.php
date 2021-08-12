d5b62d259d916a83886ef377861130faf7e0773d
kwong25 <kwong25@u.rochester.edu>
2021-08-12 01:18:06 -0400

    placed guest book forms and content into container

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php 
 include "inc/top-part.php"; 
 ?>
-
+<div class="container">
 <header>
 	<h1>Visit our Cities</h1>
 </header>
@@ -10,4 +10,6 @@ include "inc/top-part.php";
 include "inc/nav.php"; 
 ?>
 <?php include "scripts/insert.php";?>
+
+</div>
 <?php include "inc/bottom-part.php"; ?>
--- visitors.php
+++ visitors.php
@@ -2,49 +2,51 @@
 include "inc/top-part.php"; 
 ?>
 
-<header>
-  <h1>Visit our Cities</h1>
-</header>
+<div class="container">
+  <header>
+    <h1>Visit our Cities</h1>
+  </header>
 
-<?php 
-include "inc/nav.php"; 
-?>
-<?php
-// connect to the database
-include('scripts/config.php');
+  <?php 
+  include "inc/nav.php"; 
+  ?>
+  <?php
+  // connect to the database
+  include('scripts/config.php');
 
-// get results from database
-$result = mysqli_query($connection, "SELECT * FROM guestbook");
-?>
+  // get results from database
+  $result = mysqli_query($connection, "SELECT * FROM guestbook");
+  ?>
 
-<h2>Our Guest Book</h2>
-<p>See who has visited Chicago, Phoenix, or Ridgewood</p>
+  <h2>Our Guest Book</h2>
+  <p>See who has visited Chicago, Phoenix, or Ridgewood</p>
 
-<table class="readtable">
-  <tr>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Zipcode</th>
-    <th>City Visited</th>
-    <th colspan="2">Functions</th>
-  </tr>
-<?php
-// loop through results of database query, displaying them in the table
-while($row = mysqli_fetch_array( $result )) {
-?>
-  <tr>
-    <td><?php echo $row['firstName']; ?></td>
-    <td><?php echo $row['lastname']; ?></td>
-    <td><?php echo $row['phoneNumber']; ?></td>
-    <td><?php echo $row['city']; ?></td>
-    <td><a href="update.php?count=<?php echo $row['count']; ?>">Edit</a></td>
-    <td><a onclick="return confirm('Are you sure you want to delete count: <?php echo $row["count"]; ?>?')" href="scripts/delete.php?count=<?php echo $row['count']; ?>">Delete</a></td> 
-  </tr>
-<?php
-// close the loop
-}
-?>
-</table>
+  <table class="readtable">
+    <tr>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Zipcode</th>
+      <th>City Visited</th>
+      <th colspan="2">Functions</th>
+    </tr>
+  <?php
+  // loop through results of database query, displaying them in the table
+  while($row = mysqli_fetch_array( $result )) {
+  ?>
+    <tr>
+      <td><?php echo $row['firstName']; ?></td>
+      <td><?php echo $row['lastname']; ?></td>
+      <td><?php echo $row['phoneNumber']; ?></td>
+      <td><?php echo $row['city']; ?></td>
+      <td><a href="update.php?count=<?php echo $row['count']; ?>">Edit</a></td>
+      <td><a onclick="return confirm('Are you sure you want to delete count: <?php echo $row["count"]; ?>?')" href="scripts/delete.php?count=<?php echo $row['count']; ?>">Delete</a></td> 
+    </tr>
+  <?php
+  // close the loop
+  }
+  ?>
+  </table>
+</div>
 <?php include "inc/bottom-part.php"; ?>
 <?php
   mysqli_free_result($result);
